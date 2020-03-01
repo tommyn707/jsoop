3f685e2c8ab03a1a0f122b1bce8d0f5ac01f46f7
tommyn707 <tommy.n707@gmail.com>
2020-03-01 12:46:41 -0800

    first commit

--- Hundredacre.js
+++ Hundredacre.js
@@ -44,3 +44,54 @@ eeyore.south = kanga;
 eeyore.east = heffa;
 
 heffa.west = eeyore;
+
+var player = {
+    location: tigger
+}
+
+function move(dir){
+    if(dir == "north" || dir == "North"){
+        if(player.location.north == null){
+            console.log("You attempt to head North, but unfortunately the path is blocked.")
+        }
+        else {
+            player.location = player.location.north;
+            console.log("You head North and arrive at " + player.location.character + "'s house!");
+        }
+    }
+    if(dir == "south" || dir == "South"){
+        if(player.location.south == null){
+            console.log("You attempt to head South, but unfortunately the path is blocked.")
+        }
+        else {
+            player.location = player.location.south;
+            console.log("You head South and arrive at " + player.location.character + "'s house!");
+        }
+    }
+    if(dir == "east" || dir == "East"){
+        if(player.location.east == null){
+            console.log("You attempt to head East, but unfortunately the path is blocked.")
+        }
+        else {
+            player.location = player.location.east;
+            console.log("You head East and arrive at " + player.location.character + "'s house!");
+        }
+    }
+    if(dir == "west" || dir == "West"){
+        if(player.location.west == null){
+            console.log("You attempt to head West, but unfortunately the path is blocked.")
+        }
+        else {
+            player.location = player.location.west;
+            console.log("You head West and arrive at " + player.location.character + "'s house!");
+        }
+    }
+}
+
+move("north");
+move("west");
+move("north");
+move("east");
+move("north");
+move("north");
+move("east");
